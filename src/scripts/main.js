eb3c87097aeae032859be3468de22c3e7be6fc5d
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-04-05 10:51:47 +0300

    add game for mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,8 @@ const game = new Game();
 
 const start = document.querySelector('.start');
 const goodLuck = document.querySelector('.message-start');
+let touchStartX = 0;
+let touchStartY = 0;
 
 start.addEventListener('click', (e) => {
   if (e.target.textContent === 'Restart') {
@@ -55,6 +57,41 @@ document.addEventListener('keydown', (e) => {
 
     if (e.key === 'ArrowLeft') {
       game.moveLeft();
+      game.getScore();
+      game.getStatus();
+    }
+  }
+});
+
+document.addEventListener('touchstart', (e) => {
+  touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
+});
+
+document.addEventListener('touchend', (e) => {
+  const touchEndX = e.changedTouches[0].clientX;
+  const touchEndY = e.changedTouches[0].clientY;
+
+  const dx = touchEndX - touchStartX;
+  const dy = touchEndY - touchStartY;
+
+  if (Math.abs(dx) > Math.abs(dy)) {
+    if (dx > 30) {
+      game.moveRight();
+      game.getScore();
+      game.getStatus();
+    } else if (dx < -30) {
+      game.moveLeft();
+      game.getScore();
+      game.getStatus();
+    }
+  } else {
+    if (dy > 30) {
+      game.moveDown();
+      game.getScore();
+      game.getStatus();
+    } else if (dy < -30) {
+      game.moveUp();
       game.getScore();
       game.getStatus();
     }
